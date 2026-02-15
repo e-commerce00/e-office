2f4877424b772f442376fadb81deba0d44d09d6d
Imran02-ops <imranxitkj@gmail.com>
2026-02-16 01:13:49 +0700

    fix final

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
 {
     if (config('app.env') === 'production') {
-        URL::forceScheme('http');
+        URL::forceScheme('https');
     }
 }
 
